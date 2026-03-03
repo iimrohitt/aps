0ff64e210a04ba1ae6446c29d8ad79941f7d6105
Rohit Mahto <iimrohitt@gmail.com>
2026-03-04 02:00:16 +0530

    toggle hide

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import ThemeToggle from "./components/ui/ThemeToggle";
 
 const App = () => {
+  const location = useLocation();
+
   return (
     <div className="relative min-h-screen">
-
       <AppRoutes />
-
 
       {location.pathname === "/" && (
         <div className="fixed bottom-6 right-6 z-50">
           <ThemeToggle />
         </div>
       )}
-
     </div>
   );
 };
